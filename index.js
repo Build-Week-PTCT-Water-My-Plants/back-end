f605e04826fa527aa46b83944dd9659cd28873c0
Marina Monsivais <minamonsi@me.com>
2021-09-15 18:18:47 -0500

    WIP: Setting up server

---
+++ index.js
@@ -0,0 +1,18 @@
+const dotenv = require("dotenv").config()
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use("/api/*", (_,res)=>{
+    res.json({data:"The API is working!"})
+})
+
+app.listen(port, ()=> {
+    console.log(`Server running on port ${port}`)
+})
+
+console.log("It's alive")
+console.log(__dirname)//directory name of the folder where index.js is found. It is important because it allows us to know which directory heroku is using during deployment
+console.log(__filename)
+console.log(process.env.USER)
+console.log(process.env.PORT)
