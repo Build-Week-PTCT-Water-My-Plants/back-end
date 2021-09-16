14d1a8ad0b35a4943e107b57a5428e3b9a7cc6b2
Marina Monsivais <minamonsi@me.com>
2021-09-16 16:44:46 -0500

    Structure of plantRouter in place is still a work in progess

--- api/server.js
+++ api/server.js
@@ -1,19 +1,25 @@
 const express = require('express');
+//Middleware Libraries
 const helmet = require('helmet');
 const morgan = require('morgan');
 const cors = require('cors');
+
+//Server created
 const server = express();
+
+//plantRouter import
+const plantRouter = require('../routes/plantRouter')
+server.use('/plants',plantRouter);
 
 server.use(express.json());
 server.use(helmet());
 server.use(morgan("dev"));
 server.use(cors());
 
-
+//Test endpoints
 server.use("/api/*", (_,res)=>{
     res.json({message:'Hello from the server side!'})
  })
-
 const currentTime = new Date().toLocaleTimeString();
 server.get('/status', (req,res)=>{
     res.status(200).json({
@@ -21,5 +27,7 @@ server.get('/status', (req,res)=>{
         message: `Server is up and running at ${currentTime}`
     })
 })
+
+
 
 module.exports = server
---
+++ routes/plantRouter.js
@@ -0,0 +1,39 @@
+const server = require('../api/server');
+
+const router = require('express').Router();
+
+router.get('/', (req,res) => {
+    res.send("getting some plants")
+});
+
+router.get('/:id', (req,res) => {
+    res.send("getting plant id")
+});
+
+router.post('/', (req,res) =>{
+    res.send("creating new plant post")
+})
+
+router.put('/:id', (req,res) => {
+    try{
+        if(exists){
+            res.send('New plant created')
+        }else{
+            res.status(404).json({message:"Could not find plant with ID"})
+        }
+    }catch (error) {
+        res.status(500).json({message:"Failed to update"})
+    }
+})
+
+router.delete('/:id', async(req,res) => {
+    try{
+        await remove(id)
+        res.json({message:"Plant deleted"})
+    }catch(error){
+        res.status(500).json({message:"Failed to delete plant"})
+    }
+})
+
+
+module.exports = router;
