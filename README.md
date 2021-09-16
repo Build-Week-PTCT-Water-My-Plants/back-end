c45320611493429067b6a995949c6010d9e24d20
Marina Monsivais <minamonsi@me.com>
2021-09-15 20:49:10 -0500

    Added folders & files

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# back-end
+Water MY Plant API
+
+Endpoints
+
+| Request | URL               | Description                 |
+| ------- | ----------------- | --------------------------- |
+| POST    | api/auth/register | Registers new user          |
+| POST    | api/auth/login    | login as an existing user   |
+| POST    | api/plants        | add plant to logged in user |
+| GET     | api/plants        | get all plants              |
+| GET     | api/plants/:id    | get specific plant by id    |
+| PUT     | api/plants/:id    | edit specific plant         |
+| DELETE  | api/plants/:id    | delete specific plant       |
--- index.js
+++ index.js
@@ -2,22 +2,19 @@ const dotenv = require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
 
-const app = express()
+const server = express()
 const port = process.env.PORT || 5000
 
-app.use(cors())
-app.use(express.json())
+const secret = process.env
+server.use(cors())
+server.use(express.json())
 
-app.use("/api/*", (_,res)=>{
+server.use("/api/*", (_,res)=>{
     res.json({data:"The API is working!"})
 })
 
-app.listen(port, ()=> {
+server.listen(port, ()=> {
     console.log(`Server running on port ${port}`)
 })
 
 console.log("It's alive")
-console.log(__dirname)//directory name of the folder where index.js is found. It is important because it allows us to know which directory heroku is using during deployment
-console.log(__filename)
-console.log(process.env.USER)
-console.log(process.env.PORT)
